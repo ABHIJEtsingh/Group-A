fd775ccdd4d5ab9f7fb311e98588f9d192f7bc93
Dwivedi Priya <pd7668665@gmail.com>
2024-10-16 02:46:59 +0530

    Nav

--- Group-A/src/App.jsx
+++ Group-A/src/App.jsx
@@ -1,11 +1,14 @@
 
 import React from 'react'
+import NavBar from './components/Nav'
 
 function App() {
 
   return (
     <>
-      
+    <div className="w-full h-screen bg-zinc-300">
+      <NavBar/>
+    </div>
     </>
   )
 }
